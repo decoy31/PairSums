e7a6cda649abcfb68842a2216f1ef61d56396147
decoy31 <decoy31@hotmail.com>
2013-06-05 22:04:14 -0700

    Made a number of improvements to the PairSums class.

--- js/PairSums.js
+++ js/PairSums.js
@@ -1,31 +1,80 @@
 "use strict";
 
 function PairSums (min, max) {
-    this.randomNumberGenerator = new RandomNumberGenerator(min, max);
+    this._randomNumberGenerator = new RandomNumberGenerator(min, max);
+
+    (function (instance) {
+        Object.defineProperties(instance, {
+            "min": {
+                get: function () {
+                    return instance._randomNumberGenerator.min;
+                },
+                set: function (val) {
+                    instance._randomNumberGenerator.min = val;
+                }
+            },
+            "max": {
+                get: function () {
+                    return instance._randomNumberGenerator.max;
+                },
+                set: function (val) {
+                    instance._randomNumberGenerator.max = val;
+                }
+            }
+        });
+    }(this));
+
 }
+
 PairSums.prototype = {
     initialize: function (size) {
-        this.randomNumbers = this.randomNumberGenerator.generateMultiple(size);
+        var randomNumbersWatch = (new StopWatch()).start();
+        var randomNumbersSortedWatch = (new StopWatch()).start();
+
+        this.randomNumbers = this._randomNumberGenerator.generateMultiple(size);
+        this.randomNumbersSorted = this.randomNumbers.slice();
+        this.randomNumbers.timeToComplete = randomNumbersWatch.stop();
+
+        if (this.randomNumbers.length) {
+            this.randomNumbersSorted = this.randomNumbersSorted.sort(function (a, b) {
+                return a - b;
+            });
+
+            this.randomNumbersSorted.timeToComplete = randomNumbersSortedWatch.stop();
+        }
+
+        return this;
     },
+
     findPairs: function (targetSum) {
+        // TODO: Implement.
+        return this.findPairsSorted(targetSum);
+    },
+
+    findPairsSorted: function (targetSum) {
         var pairs = [],
             x = 0,
-            y = this.randomNumbers.length - 1;
+            y = this.randomNumbersSorted.length - 1;
 
         targetSum = parseInt(targetSum, 10) || 0;
 
+        var watch = (new StopWatch()).start();
+
         // Assumption: Array is sorted.
         while (x < y) {
-            if (this.randomNumbers[x] + this.randomNumbers[y] === targetSum) {
-                pairs.push([this.randomNumbers[x], this.randomNumbers[y]]);
-                x++;
+            if (this.randomNumbersSorted[x] + this.randomNumbersSorted[y] === targetSum) {
+                pairs.push([this.randomNumbersSorted[x], this.randomNumbersSorted[y]]);
                 y--;
-            } else if (this.randomNumbers[x] + this.randomNumbers[y] < targetSum) {
+            } else if (this.randomNumbersSorted[x] + this.randomNumbersSorted[y] < targetSum) {
                 x++;
-            } else if (this.randomNumbers[x] + this.randomNumbers[y] > targetSum) {
+            } else if (this.randomNumbersSorted[x] + this.randomNumbersSorted[y] > targetSum) {
                 y--;
             }
         }
+
+        pairs.timeToComplete = watch.stop();
+
+        //console.log('findPairsSorted completed in ' + pairs.timeToComplete + 'ms');
 
         return pairs;
     }
